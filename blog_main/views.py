9481760d06d1869c5d7fa5ef63431b9f9d902377
Yadnyesh2003 <yfirke2003@gmail.com>
2025-12-16 00:29:16 +0530

    Login & Registration functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from blog_main.forms import RegistrationForm
 from blogs.models import Blog, Category
 from miscellaneous.models import About
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 
@@ -39,3 +41,28 @@ def register(request):
         'form': form,
     }
     return render(request, 'register.html', context)
+
+
+def login(request):
+    if(request.method == 'POST'):
+        form = AuthenticationForm(request, request.POST)
+        if(form.is_valid()):
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            print(form.errors)
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
