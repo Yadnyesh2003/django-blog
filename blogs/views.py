55198d8b2a40a25f87c631a5784bbecf3f1d7fa7
Yadnyesh2003 <yfirke2003@gmail.com>
2025-12-17 18:16:58 +0530

    Applying latest changes, Added Dashboard app and creating dashboard. Start from login decorator part of the video

--- blogs/views.py
+++ blogs/views.py
@@ -32,8 +32,15 @@ def blogs(request, slug):
     # return render(request, 'blogs.html')
 
 def search(request):
-    keyword = request.GET.get('keyword')
-    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status="Published")
+    keyword = request.GET.get('keyword', '').strip()
+    blogs = Blog.objects.none()
+    if keyword:
+        blogs = Blog.objects.filter(
+            Q(title__icontains=keyword) |
+            Q(short_description__icontains=keyword) |
+            Q(blog_body__icontains=keyword),
+            status="Published"
+        )
     context = {
         'blogs': blogs,
         'keyword': keyword,
---
+++ dashboards/views.py
@@ -0,0 +1,16 @@
+from multiprocessing import context
+from blogs.models import Blog, Category
+from django.shortcuts import render
+
+# Create your views here.
+
+def dashboard(request):
+    category_count = Category.objects.all().count()
+    print(category_count)
+    blogs_count = Blog.objects.all().count()
+
+    context = {
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
